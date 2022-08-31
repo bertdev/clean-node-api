60d440af48fb2eee747bd79751c829ebf2594ca3
bertdev <herbertmatos10@hotmail.com>
2022-08-31 08:20:01 -0300

    format  using standard js

--- login.js
+++ login.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
-const AccountModel = mongoose.model('Account');
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
 
 module.exports = () => {
-  const router = new SignUpRouter()
-  router.post('/signup', ExpressRouterAdapter.adapt(router));
+  const route = new SignUpRouter()
+  router.post('/signup', ExpressRouterAdapter.adapt(route))
 }
 
 class ExpressRouterAdapter {
-  static adapt(router) {
+  static adapt (router) {
     return async (req, res) => {
       const httpRequest = {
         body: req.body
       }
-      const httpResponse = await router.route(httpRequest);
-      res.status(httpResponse.statusCode).json(httpResponse.boby);
+      const httpResponse = await router.route(httpRequest)
+      res.status(httpResponse.statusCode).json(httpResponse.boby)
     }
   }
 }
@@ -23,9 +23,9 @@ class ExpressRouterAdapter {
 // Presentation layer
 // signup-router
 class SignUpRouter {
-  async route(httpRequest) {
-    const { email, password, repeatPassword } = httpRequest.body;
-    const user = new SignUpUseCase().signUp(email, password, repeatPassword);
+  async route (httpRequest) {
+    const { email, password, repeatPassword } = httpRequest.body
+    const user = new SignUpUseCase().signUp(email, password, repeatPassword)
     return {
       statusCode: 200,
       body: user
@@ -36,7 +36,7 @@ class SignUpRouter {
 // Domain layer
 // signup-usecase
 class SignUpUseCase {
-  async signUp(email, password, repeatPassword) {
+  async signUp (email, password, repeatPassword) {
     if (password === repeatPassword) {
       new AddAccountRepository().add(email, password)
     }
@@ -46,8 +46,8 @@ class SignUpUseCase {
 // Infra layer
 // signup-account-repository
 class AddAccountRepository {
-  async add(email, password) {
-    const user = await AccountModel.create({ email, password });
-    return user;
+  async add (email, password) {
+    const user = await AccountModel.create({ email, password })
+    return user
   }
 }
