1ffa6378f94216ddee3cfd55540f2520e2ee22c3
bertdev <herbertmatos10@hotmail.com>
2022-09-11 11:34:50 -0300

    enable cors

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -3,11 +3,22 @@ const app = require('./app')
 
 describe('App Setup', () => {
   test('Should disable x-powered-by header', async () => {
-    app.get('/ping', (req, res) => {
-      res.send('pong')
+    app.get('/test_x_powered_by', (req, res) => {
+      res.send('')
     })
     const response = await request(app)
-      .get('/ping')
+      .get('/test_x_powered_by')
     expect(response.headers['x-powered-by']).toBeUndefined()
+  })
+
+  test('Should enable cors', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send('')
+    })
+    const response = await request(app)
+      .get('/test_cors')
+    expect(response.headers['access-control-allow-origin']).toBe('*')
+    expect(response.headers['access-control-allow-methods']).toBe('*')
+    expect(response.headers['access-control-allow-headers']).toBe('*')
   })
 })
