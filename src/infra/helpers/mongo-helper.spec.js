9921aeb889e701f4754a83c6405491dfdc8df7df
bertdev <herbertmatos10@hotmail.com>
2022-09-11 14:39:39 -0300

    ensure MongoHelper reconnect when getCollection is invoked and client is disconnected

---
+++ src/infra/helpers/mongo-helper.spec.js
@@ -0,0 +1,23 @@
+const MongoHelper = require('./mongo-helper')
+
+describe('Mongo Helper', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(global.__MONGO_URI__)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return a collection', async () => {
+    const collection = await MongoHelper.getCollection('users')
+    expect(collection).toBeTruthy()
+  })
+
+  test('Should return a reconnect when getCllection() is invoked and client is disconnected', async () => {
+    await MongoHelper.disconnect()
+    expect(MongoHelper.client).toBeFalsy()
+    await MongoHelper.getCollection('users')
+    expect(MongoHelper.client).toBeTruthy()
+  })
+})
